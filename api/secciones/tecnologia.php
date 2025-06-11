85307c4a858e4649a2ab5984858a4e082e2aab7f
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-06-11 10:18:34 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿Usas firmas digitales? Evita robos cibernéticos y toma estas recomendaciones",
+"autor" => "Carlos Rosales Salas",
+"resumen" => "En Gestión, valoramos profundamente la labor periodística que realizamos para mantenerlos informados. Por ello, les recordamos que no está permitido, reproducir, comercializar, distribuir, copiar total o parcialmente los contenidos que publicamos en nuestra web, sin autorizacion previa y expresa de Empresa Editora El Comercio S.A.",
 ];
 ?>
