db1df35f07f7c54880c4a2025977009356d771e9
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-06-11 10:16:59 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La exploración de litio no avanza, ¿el boom del ‘oro blanco’ llegó a su fin?",
+"autor" => "Juan Saldarriaga",
+"resumen" => "El próximo mes se cumplirán siete años desde que la minera Macusani Yellowcake revelase la existencia, en la región Puno, de la denominada la “mina de litio más grande del mundo”: el proyecto Falchani.",
 ];
 ?>
