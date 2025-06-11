d3034687df62a84354244f72b75b4e8bf1a38463
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-06-11 10:13:56 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Portugal Cmapeón de la UEFA Nations League 2025",
+"autor" => "Marca",
+"resumen" => "La selección portuguesa salió campeona del reciente evento deportivo de selecciones europeas.",
 ];
 ?>
