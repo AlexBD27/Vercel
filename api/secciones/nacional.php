97048e164bafa12ceb5f4d4112d324e46a1d42e6
AlexBD27 <109763600+AlexBD27@users.noreply.github.com>
2025-06-11 10:15:13 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Un explosivo detonó en obra de la Estación Central de la Línea 2 del Metro de Lima y dañó parte de cerco perimétrico",
+"autor" => "Sergio Espinoza",
+"resumen" => "La Policía descartó que se trate de un caso de extorsión contra la empresa concesionaria, pero señaló que ya analiza las cámaras de seguridad para hallar a quienes detonaron el explosivo.",
 ];
 ?>
